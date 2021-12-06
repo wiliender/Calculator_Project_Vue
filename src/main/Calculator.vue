dc230242e6ed08d0e81d9e7cadead18fee1eb1a9
Wiliender Ferreira Silva <wiliendersilva@gmail.com>
2021-12-06 15:15:35 -0300

    implementing the logic

--- src/main/Calculator.vue
+++ src/main/Calculator.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="calculator">
-        <Display value="1000" />
+        <Display :value="displayValue" />
         <Button label="AC" triple @onClick="clearMemory" />
         <Button label="/" operation @onClick="setOperation" />
         <Button label="7" @onClick="addDigit" />
@@ -27,18 +27,73 @@ import Display from "../components/Display"
 import Button from "../components/Button"
 
 export default {
+    data: function() {
+        return{
+            displayValue: "0",//valor que será mostrado na tela
+            clearDisplay: false,//sempre que o display for atualizado, ele deve ser limpo
+            operation: null,//operação que será executada
+            values: [0, 0],//armazena os valores que serão usados para realizar a operação
+            current: 0//indica qual o valor que será usado para realizar a operação
+        }
+    },
     components: {
         Display,
         Button
     },
     methods: {
         clearMemory() {
+            Object.assign(this.$data, this.$options.data())//Volta o objeto para o estado inicial
             //console.log("Limpar Memória!")
         },
         setOperation(operation) {
+            if(this.current === 0) {
+                this.operation = operation
+                this.current = 1
+                this.clearDisplay = true
+            } else {
+                const equals = operation.label === "="
+                const currentOperation = this.operation
+
+                try {
+                    this.values[0] = eval(
+                        `${this.values[0]} ${currentOperation} ${this.values[1]}`
+                    )
+                } catch (e) {
+                    this.emit("onError", e)
+                }
+
+                this.values[1] = 0//limpa o segundo valor para uma proxima operação
+
+                this.displayValue = this.values[0]//resultado a operação sera mostrado no display
+                this.operation = equals ? null : operation
+                this.current = equals ? 0 : 1
+                this.clearDisplay = !equals
+            }
             //console.log("Operação: " + operation)
         },
         addDigit(n) {
+            if(n == "." && this.displayValue.includes(".")) {
+                return
+            }
+
+            const clearDisplay = this.displayValue == "0"
+                || this.clearDisplay
+            const currentValue = clearDisplay ? "" : this.displayValue//se o display estiver limpo, não precisa concatenar
+            const displayValue = currentValue + n//concatena o valor que será mostrado na tela
+
+            this.displayValue = displayValue//atualiza o valor que será mostrado na tela
+            this.clearDisplay = false//limpa o display
+            
+            // Alternativa 1
+            this.values[this.current] = displayValue//atualiza o valor que será usado para realizar a operação
+
+            // Alternativa 2
+            //if (n !== ".") {
+            //    const i = this.current//indica qual o valor que será usado para realizar a operação
+            //    const newValue = parseFloat(displayValue)//converte o valor que será usado para realizar a operação
+            //    this.values[i] = newValue//atualiza o valor que será usado para realizar a operação
+            //}
+
             //console.log("Número: " + n)
         }
     }
